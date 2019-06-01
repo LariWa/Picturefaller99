1446970cd6205f8f89dd621ab3463ae810de29d8
LaWa15 <larissa.wagnerberger@gmail.com>
2019-06-01 15:30:35 +0200

    Update HighscoreTable.cs

--- PictureFaller3/Assets/Scripts/HighscoreTable.cs
+++ PictureFaller3/Assets/Scripts/HighscoreTable.cs
@@ -1,14 +1,4 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
+﻿//Tutorial: https://www.youtube.com/watch?v=iAbaqGYdnyI
 
 using System.Collections;
 using System.Collections.Generic;
