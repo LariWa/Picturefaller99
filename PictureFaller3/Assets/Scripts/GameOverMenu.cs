d721fc1e6e9902c07946ffbea66fce4a9700fdc0
LaWa15 <40071882+LaWa15@users.noreply.github.com>
2019-06-05 19:08:02 +0200

    sore to highscore

--- PictureFaller3/Assets/Scripts/GameOverMenu.cs
+++ PictureFaller3/Assets/Scripts/GameOverMenu.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
     public GameObject scoreText;
+    public Text name;
+
+    public HighscoreTable highscoreTable;
+
     public void PlayGame () 
     {
         SceneManager.LoadScene("World01big");
@@ -14,4 +20,20 @@ public class GameOverMenu : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void submit()
+    {
+       highscoreTable = new HighscoreTable();
+        string Name = name.text;
+        Debug.Log(Name);
+        Debug.Log(scoreText.transform.GetComponent<TextMeshProUGUI>().text);
+        string score = scoreText.transform.GetComponent<TextMeshProUGUI>().text;
+        //int score = int.Parse(scoreText.transform.GetComponent<TextMeshProUGUI>().text);
+
+        highscoreTable.AddHighscoreEntry(10284934, "Mert");
+        Debug.Log(score);
+        Debug.Log(Name);
+
+    }
+
 }
