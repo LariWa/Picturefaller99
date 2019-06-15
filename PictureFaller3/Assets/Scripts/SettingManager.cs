5357150b66243f705a5e50e647b42a03cccef93c
JodiePreuss <43951054+JodiePreuss@users.noreply.github.com>
2019-06-15 16:04:40 +0200

    MountainWorld

--- PictureFaller3/Assets/Scripts/SettingManager.cs
+++ PictureFaller3/Assets/Scripts/SettingManager.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 
 public class SettingManager : MonoBehaviour
 {
-    public enum Settings { City, Forest, Food};
+    public enum Settings { City, Forest, Food, Mountain};
     [SerializeField] private bool startRandom;
     [SerializeField] private Settings startSetting;
     private Settings currentSetting; //For chunks
@@ -14,6 +14,7 @@ public class SettingManager : MonoBehaviour
     [SerializeField] private GameObject[] cityChunks;
     [SerializeField] private GameObject[] forestChunks;
     [SerializeField] private GameObject[] foodChunks;
+    [SerializeField] private GameObject[] mountainChunks;
 
     [Space]
 
@@ -21,16 +22,19 @@ public class SettingManager : MonoBehaviour
     [SerializeField] private Sprite[] allCityPictures;  //original order
     [SerializeField] private Sprite[] allForestPictures;//original order
     [SerializeField] private Sprite[] allFoodPictures;//original order
+    [SerializeField] private Sprite[] allMountainPictures;//original order
 
     private Sprite[] cityPicturesInSort;  //Sorted always differently
     private Sprite[] forestPicturesInSort;//Sorted always differently
     private Sprite[] foodPicturesInSort;//Sorted always differently
+    private Sprite[] mountainPicturesInSort;//Sorted always differently
 
     [Space]
 
     [SerializeField] private TextAsset[] citySorts;
     [SerializeField] private TextAsset[] forestSorts;
     [SerializeField] private TextAsset[] foodSorts;
+    [SerializeField] private TextAsset[] mountainSorts;
 
     public bool useSorts;
     private DifficultyManager difficultyManager;
@@ -165,6 +169,7 @@ public class SettingManager : MonoBehaviour
         if (s == Settings.City) return citySorts;
         if (s == Settings.Forest) return forestSorts;
         if (s == Settings.Food) return foodSorts;
+        if (s == Settings.Mountain) return mountainSorts;
 
         return null;
     }
@@ -174,6 +179,7 @@ public class SettingManager : MonoBehaviour
         if (s == Settings.City) return allCityPictures;
         if (s == Settings.Forest) return allForestPictures;
         if (s == Settings.Food) return allFoodPictures;
+        if (s == Settings.Mountain) return allMountainPictures;
 
         return null;
     }
@@ -185,6 +191,7 @@ public class SettingManager : MonoBehaviour
         if (s == Settings.City) return cityPicturesInSort;
         if (s == Settings.Forest) return forestPicturesInSort;
         if (s == Settings.Food) return foodPicturesInSort;
+        if (s == Settings.Mountain) return mountainPicturesInSort;
 
         return null;
     }
@@ -193,6 +200,7 @@ public class SettingManager : MonoBehaviour
         if (s == Settings.City) cityPicturesInSort = spr;
         if (s == Settings.Forest) forestPicturesInSort = spr;
         if (s == Settings.Food) foodPicturesInSort = spr;
+        if (s == Settings.Mountain) mountainPicturesInSort = spr;
     }
 
 
@@ -201,6 +209,7 @@ public class SettingManager : MonoBehaviour
         if (currentSetting == Settings.City) return cityChunks[Random.Range(0, cityChunks.Length)];
         if (currentSetting == Settings.Forest) return forestChunks[Random.Range(0, forestChunks.Length)];
         if (currentSetting == Settings.Food) return foodChunks[Random.Range(0, foodChunks.Length)];
+        if (currentSetting == Settings.Mountain) return mountainChunks[Random.Range(0, mountainChunks.Length)];
 
         return null;
     }
