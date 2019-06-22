face4073d2d3acbb7ed679019ceb9c396bf998c8
vbondarenko1 <31034418+vbondarenko1@users.noreply.github.com>
2019-06-22 13:35:39 +0200

    menu updated

---
+++ PictureFaller3/Assets/MainMenuCharMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCharMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float range;
+    private Rigidbody rb;
+    private Vector3 direction;
+    Vector3 originalPos;
+    Vector3 constraintPos;
+    bool oben;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        range = 1f;
+        //originalPos = rb.Get;
+        constraintPos = new Vector3(0, range, range * 1 / 3);
+        oben = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 1f, 1), transform.position.z);
+    }
+
+    void ChangePosition()
+    {
+        transform.position = direction;
+        //Compute position for next time
+        direction = new Vector3(0, Random.Range(-1, 1), 0);
+    }
+}
