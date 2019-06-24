1e0a0a8c6f2c501c495db7bbd8b32cd386c31294
vbondarenko1 <31034418+vbondarenko1@users.noreply.github.com>
2019-06-24 20:19:56 +0200

    Intro scene setup

---
+++ PictureFaller3/Assets/Scripts/SleepAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepAnimation : MonoBehaviour
+{
+    private float finalPositionRotation;
+    private float finalPositionTranslation;
+
+    private Vector3 goalPosition;
+    private Vector3 goalRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalPosition = new Vector3(0.064f, -0.717f, -0.649f);
+        goalRotation = new Vector3(-73.11f, 86.805f, 3.058f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Animate the player: position and rotation
+        Debug.Log(transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, goalPosition, 1f*Time.deltaTime);
+        Vector3 NewDir = Vector3.RotateTowards(transform.position, goalRotation, 1f*Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(NewDir);
+    }
+}
