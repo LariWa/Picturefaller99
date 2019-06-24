980627fcc8d17b13c779acb753a952aa674e8e61
vbondarenko1 <31034418+vbondarenko1@users.noreply.github.com>
2019-06-24 17:53:28 +0200

    Main Menu refectored into Menu Controller

---
+++ PictureFaller3/Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    //Play Game is being used in Main Menu
+    public void PlayGame()
+    {
+        print("changeScene");
+        SceneManager.LoadScene("World01big");
+    }
+
+    public void MoveCharacter() {
+
+        //if the script is connected to a player character, "animate" the character (this is used in main menu only)
+        if (this.tag == "Player")
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 1f, 1), transform.position.z);
+        }
+    }
+}
