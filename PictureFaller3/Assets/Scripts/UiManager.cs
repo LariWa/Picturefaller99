8ca125238f8029ee45dc73b08e6f00a5b4db5862
Nito <nikolas1997@hotmail.de>
2019-07-04 20:36:06 +0200

    Better Portal + Transition

--- PictureFaller3/Assets/Scripts/UiManager.cs
+++ PictureFaller3/Assets/Scripts/UiManager.cs
@@ -15,9 +15,10 @@ public class UiManager : MonoBehaviour
     //public int portalScaleVib = 10;
     //public float portalScaleRand = 90;
 
-    public float portalScaleDur = 1;
+    public float portalScaleShrinkDur = 1;
+    public float portalScaleExpandDur = 1;
     public float portalScalePunch = 0.5f;
-    private Vector3 punchVec;
+    private Vector3 smallScale;
     public int portalScaleVib = 10;
     public float portalScaleElast = 1;
 
@@ -25,10 +26,16 @@ public class UiManager : MonoBehaviour
 
     void Start()
     {
-        punchVec = new Vector3(portalScalePunch, portalScalePunch, portalScalePunch);
+        smallScale = new Vector3(portalScalePunch, portalScalePunch, portalScalePunch);
+
+        //portal.transform.DORotate(new Vector3(0,0,transform.rotation.z + 1), portalRotateSpeed).SetLoops(-1);
+
 
         //portal.transform.DOShakeScale(portalScaleDur, portalScaleStr, portalScaleVib, portalScaleRand); //SetLoops(-1)
-        portal.transform.DOPunchScale(punchVec, portalScaleDur, portalScaleVib, portalScaleElast).SetLoops(-1);
+        //portal.transform.DOPunchScale(punchVec, portalScaleDur, portalScaleVib, portalScaleElast).SetLoops(-1);
+        Sequence seq = DOTween.Sequence().SetLoops(-1);
+        seq.Append(portal.transform.DOScale(smallScale, portalScaleShrinkDur).SetEase(Ease.InOutSine));
+        seq.Append(portal.transform.DOScale(portal.transform.localScale, portalScaleExpandDur).SetEase(Ease.Linear));
     }
 
 
